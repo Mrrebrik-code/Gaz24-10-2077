9c28a193c24793556dceb14ab54988e2ca8fdbec
Mrrebrik <itibsoft@gmail.com>
2021-04-04 13:43:49 +0300

    Edit physics car

---
+++ Gaz24-10-2077/Assets/Simple Realistic Helicopter Physics/scripts/SendBreakeMenssage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+public class SendBreakeMenssage : MonoBehaviour {
+
+	public Helicopter helicopterScript;
+	public AudioSource audSource;
+
+	void OnTriggerEnter (Collider col)
+	{
+		helicopterScript.Breaked ();
+		if(audSource.isPlaying == false)
+		{
+			audSource.Play ();
+		}
+	}
+}
